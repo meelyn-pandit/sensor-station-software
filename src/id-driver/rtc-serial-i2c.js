607257cbb3e92db97bc4948d656e0782038bd3a7
Bob Fogg <philomachus@gmail.com>
2022-04-28 10:41:56 -0400

    fix tabs rtc serial

--- src/id-driver/rtc-serial-i2c.js
+++ src/id-driver/rtc-serial-i2c.js
@@ -3,17 +3,17 @@ import 'regenerator-runtime/runtime'
 
 class RtcSerial {
 	constructor() {
-		this.i2c_address = 0x57
+    this.i2c_address = 0x57
     this.register = 0xf0
-	}
+  }
 
-	async getId() {
-		let buffer = Buffer.alloc(8)
-		const i2c1 = await i2c.openPromisified(1)
+  async getId() {
+    let buffer = Buffer.alloc(8)
+    const i2c1 = await i2c.openPromisified(1)
     await i2c1.readI2cBlock(this.i2c_address, this.register, buffer.length, buffer)
-		await i2c1.close()
-		return buffer.toString('hex').toUpperCase()
-	}
+    await i2c1.close()
+    return buffer.toString('hex').toUpperCase()
+  }
 }
 
 export { RtcSerial }
