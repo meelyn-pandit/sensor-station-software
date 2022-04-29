f1b51d0facf3f48d361ef6e61295a5ed8f5c142b
Bob Fogg <bob.fogg@celltracktech.com>
2022-04-29 08:36:29 -0400

    fixing indents

--- src/id-driver/atsha204a.js
+++ src/id-driver/atsha204a.js
@@ -1,18 +1,18 @@
 const getDeviceId = () => {
-  return new Promise((resolve, reject) =>{ 
+  return new Promise((resolve, reject) => {
     let id = ""
-    let child = exec(`hashlet serial-num`, (error,stdout, stderr) =>{ 
-      if(error){         
+    let child = exec(`hashlet serial-num`, (error, stdout, stderr) => {
+      if (error) {
         reject(error)
-      }   
-    })  
+      }
+    })
     child.stdout.on('data', (data) => {
       id += data
-    })  
+    })
     child.on('close', (code) => {
-      resolve(id.substring(4, id.length -3))
-    })    
-  })  
+      resolve(id.substring(4, id.length - 3))
+    })
+  })
 }
 
 export { getDeviceId }
