1e1626ff59c0f851d5caca514c5f77b5b45bc6c5
Bob Fogg <bob.fogg@celltracktech.com>
2022-05-26 15:09:58 -0400

    use V3 for first 2 characters of station ID

--- src/id-driver/v3-driver.js
+++ src/id-driver/v3-driver.js
@@ -1,9 +1,12 @@
 import RtcEeprom from './rtc-serial.js'
 
+const V3_PREFIX = 'V3'
+
 const get_id = async () => {
   const eeprom = new RtcEeprom()
-  const id = await eeprom.getId()
-  console.log(id)
+  let rtc_id = await eeprom.getId()
+	rtc_id = V3_PREFIX.concat(rtc_id.substring(6))
+  console.log(rtc_id)
   process.exit(0)
 }
 
