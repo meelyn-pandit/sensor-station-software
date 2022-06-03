4907b7bfd6652c553a711e247ed27fab89cdb992
Bob Fogg <bob.fogg@celltracktech.com>
2022-06-03 13:54:49 -0400

    fix radio map link - delete

--- system/scripts/save-station-id.sh
+++ system/scripts/save-station-id.sh
@@ -3,13 +3,14 @@
 /usr/local/bin/node /lib/ctt/sensor-station-software/src/id-driver/station-revision.js
 typeset -i version=$(cat /etc/ctt/station-revision)
 RADIO_MAP=/etc/ctt/radio-map.json
-if test -f $RADIO_MAP; then
+if test -h $RADIO_MAP; then
+	echo 'deleting current radio map'
 	rm $RADIO_MAP
 fi
 if test $version -ge 3; then
 	echo 'linking v3 radio map'
 	ln -s /lib/ctt/sensor-station-software/system/radios/v3-radio-map.js $RADIO_MAP
 else
-	echo 'lniking v2 radio map'
+	echo 'linking v2 radio map'
 	ln -s /lib/ctt/sensor-station-software/system/radios/v2-radio-map.js $RADIO_MAP
 fi
