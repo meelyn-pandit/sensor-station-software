2670a2f16535d42473f2e0c343cef32fc9867c13
Bob Fogg <bob.fogg@celltracktech.com>
2022-06-03 17:12:30 -0400

    update v3 radio map for dongles

--- system/radios/v3-radio-map.js
+++ system/radios/v3-radio-map.js
@@ -15,17 +15,17 @@
   "path": "/dev/serial/by-path/platform-3f980000.usb-usb-0:1.2.6:1.0"
 },{
   "channel": 6,
-  "path": "/dev/serial/by-path/platform-3f980000.usb-usb-0:1.7:1.0-port0"
+  "path": "/dev/serial/by-path/platform-3f980000.usb-usb-0:1.7:1.0"
 },{
   "channel": 7,
-  "path": "/dev/serial/by-path/platform-3f980000.usb-usb-0:1.6:1.0-port0"
+  "path": "/dev/serial/by-path/platform-3f980000.usb-usb-0:1.6:1.0"
 },{
   "channel": 8,
-  "path": "/dev/serial/by-path/platform-3f980000.usb-usb-0:1.5:1.0-port0"
+  "path": "/dev/serial/by-path/platform-3f980000.usb-usb-0:1.5:1.0"
 },{
   "channel": 9,
-  "path": "/dev/serial/by-path/platform-3f980000.usb-usb-0:1.4:1.0-port0"
+  "path": "/dev/serial/by-path/platform-3f980000.usb-usb-0:1.4:1.0"
 },{
   "channel": 10,
-  "path": "/dev/serial/by-path/platform-3f980000.usb-usb-0:1.3:1.0-port0"
+  "path": "/dev/serial/by-path/platform-3f980000.usb-usb-0:1.3:1.0"
 }]
