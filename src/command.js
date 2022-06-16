4e9c863160bc0f3c68ef1480769e8eaf36a8b917
Bob Fogg <bob.fogg@celltracktech.com>
2022-06-16 14:35:03 -0400

    command logging

--- src/command.js
+++ src/command.js
@@ -1,6 +1,7 @@
 import { exec } from 'child_process'
 
 export default async (cmd) => {
+  console.log('executing command', cmd)
   return new Promise((resolve, reject) => {
     exec(cmd, (error, stdout, stderr) => {
       if (error) {
