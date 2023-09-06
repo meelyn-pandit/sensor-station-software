056a6c8ce6339b7a06768b44a1be1781cba4e82c
Meelyn Pandit <meelyn.pandit@celltracktech.com>
2023-09-06 13:31:45 -0400

    updated open-serial-ports to read directly from radio-map file but don't really use the function anywhere...

--- src/station-radio-interface/index.js
+++ src/station-radio-interface/index.js
@@ -1,13 +1,9 @@
 import { BaseStation } from './server/base-station.js'
-// import OpenSerialPorts from './server/open-serial-ports.js'
 
-// // create radio json
-// OpenSerialPorts()
 // instantiate the base station software
 const station = new BaseStation({
   config_filepath: '/etc/ctt/station-config.json',
   radio_map_filepath: '/etc/ctt/radio-map.json'
-  // radio_map_filepath: '/home/ctt/sensor-station-software/src/station-radio-interface/server/data/serial-ports.json',
 })
 
 // start it up
--- src/station-radio-interface/server/open-serial-ports.js
+++ src/station-radio-interface/server/open-serial-ports.js
@@ -1,57 +1,73 @@
 import fs from 'fs'
 
-export default function OpenSerialPorts () {
-  console.log('open serial ports function is running')
-    let file_object = []
-    fs.readdir('../../../../dev/serial/by-path', (err, files) => {
-      console.log('save open radios files', files)
-      if (err) {
-      console.log(err)
-      } else {
-        console.log("\nCurrent directory filenames:")
-        // return files
-        files.forEach((file, i) => {
-          console.log('radio serial path', file)
-          if (file.substring(38,43) === 'port0') {
-            console.log('what is this serial path', file, file.substring(38,43))
-          } else {
-            let file_path = '/dev/serial/by-path/' + file
-            let channel = i-3
-            let file_obj = {
-              channel,
-              path: file_path,
-            }
-            file_object.push(file_obj)
-            console.log('serial port array', file_object)
-            return file_object
-          }
-        })
+export default function OpenSerialPorts() {
+  // console.log('save open radios regular radio config file',  this.config.data.radios)
+  let file_object = []
 
-    fs.writeFile("serial-ports.json", JSON.stringify(file_object),
-      {
-        encoding: "utf8",
-        flag: "w",
-        mode: 0o666
-      },
-        (err) => {
-          if (err)
-            console.log(err);
-          else {
-            console.log("File written successfully\n");
-            console.log("The written has the following contents:");
-            console.log(fs.readFileSync("serial-ports.json", "utf8"));
-          }
-        });
+  const radio_map = fs.readFileSync('../../../etc/ctt/radio-map.json', 
+    { 
+      encoding: 'utf8', 
+      flag: 'r',
+    }, 
+    (err, file) => {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log("\nOriginal Radio Map", Array.isArray(file))
       }
-    })
+  })
+  console.log('radio map', JSON.parse(radio_map), typeof radio_map)
+      fs.readdir('../../../../../../dev/serial/by-path', (err, files) => {
+        console.log('save open radios files', files)
+        if (err) {
+          console.log('serial port fs error', err)
+        } else {
+          console.log("\nCurrent directory filenames:")
+          // console.log('radio map in read directory scope', radio_map)
+
+          JSON.parse(radio_map).forEach((radio) => {
+            // console.log('radio', radio)
+            files.forEach((file) => {
+              // console.log('radio serial path', file)
+              
+              let file_path = '/dev/serial/by-path/' + file
+              if (file_path === radio.path) {
+                file_object.push(radio)
+              }
+              
+              console.log('serial port array', typeof file_object)
+              return file_object
+            })
+          })
+
+        fs.writeFile("./src/station-radio-interface/server/data/serial-ports.json", JSON.stringify(file_object),
+          {
+            encoding: "utf8",
+            flag: "w",
+            mode: 0o666
+          },
+            (err) => {
+              if (err)
+                console.log(err);
+              else {
+                console.log("File written successfully\n");
+                console.log("The written has the following contents:");
+                console.log(fs.readFileSync("./src/station-radio-interface/server/data/serial-ports.json", "utf8"));
+              }
+            });
+        }
+      })
+
     console.log('watching serial directory')
 
-    // fs.watch('../../../dev/serial/by-path', (eventType, filename) => {
-    //   console.log(`event type is: ${eventType}`);
-    //   if (filename) {
-    //     console.log(`filename provided: ${filename}`);
-    //   } else {
-    //     console.log('filename not provided');
-    //   }
-    // });
+      fs.watch('../../../dev/serial/by-path', (eventType, filename) => {
+        console.log(`event type is: ${eventType}`);
+        if (filename) {
+          console.log(`filename provided: ${filename}`);
+          // OpenSerialPorts()
+        } else {
+          console.log('filename not provided');
+        }
+      })
+      return file_object
 }
